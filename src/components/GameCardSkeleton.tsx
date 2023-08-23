9d3fb7175281887be3dd9054dcf2cbb625ccf59b
molul <luismorcillo@gmail.com>
2023-08-23 18:24:55 +0200

    Height fix for GameCardSkeleton

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton></Skeleton>
+      <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
