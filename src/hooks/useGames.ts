2a9fbbaf334bcecd38c9df64650a75c6172552c8
molul <luismorcillo@gmail.com>
2023-08-24 14:12:40 +0200

    Made a small mistake in last commit

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,7 @@ export interface Game {
   id: number;
   name: string;
 	background_image: string;
-	parent_platforms: { parent_platforms: Platform}[];
+	parent_platforms: { platform: Platform}[];
 	metacritic: number;
 	rating_top: number;
 	rating: number;
